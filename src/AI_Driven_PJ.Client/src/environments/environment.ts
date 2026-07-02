c7d4ee43b7985dfae9d82db5a81fb3d030983e68
Su War <suwar14081990@gmail.com>
2026-07-02 15:38:25 +0630

    Create environment.ts

---
+++ src/AI_Driven_PJ.Client/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    apiUrl: '',
+
+    main_url: 'https://localhost:7298',
+
+    web_url: 'https://localhost:7298',
+
+    debug: true,
+
+    default_pagination: [10, 20, 30, 50, 100],
+
+    default_toastKey: 'globalMessage'
+};
