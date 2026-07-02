80030ebd1d844b4861bdea0612c8fd15f0979611
Su War <suwar14081990@gmail.com>
2026-07-02 15:37:52 +0630

    Create app.routes.ts

---
+++ src/AI_Driven_PJ.Client/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'auth/login'
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth.routes')
+    },
+    {
+        path: '**',
+        redirectTo: 'auth/login'
+    }
+];
