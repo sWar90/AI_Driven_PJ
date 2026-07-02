1d4b725174c23e9357e8954a2006a8316faadbf0
Su War <suwar14081990@gmail.com>
2026-07-02 13:40:40 +0630

    Create ApiMessages.cs

---
+++ src/AI_Driven_PJ.Application/Common/Models/ApiMessages.cs
@@ -0,0 +1,7 @@
+namespace AI_Driven_PJ.Application.Common.Models;
+
+public static class ApiMessages
+{
+    public const string Unauthorized = "Unauthorized";
+    public const string Forbidden = "Forbidden";
+}
