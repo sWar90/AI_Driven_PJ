75d379147ee316834e7c84309a8f6bb0fa6c35c9
Su War <suwar14081990@gmail.com>
2026-07-02 13:40:37 +0630

    Update AuthConstants.cs

--- src/AI_Driven_PJ.Application/Common/Models/AuthConstants.cs
+++ src/AI_Driven_PJ.Application/Common/Models/AuthConstants.cs
@@ -3,4 +3,9 @@ namespace AI_Driven_PJ.Application.Common.Models;
 public static class AuthConstants
 {
     public const string CompanyHeader = "X-Company-Id";
+    public const string CompanyIdClaim = "company_id";
+    public const string BaseCurrencyIdClaim = "base_currency_id";
+    public const string IsSuperAdminClaim = "is_super_admin";
+    public const string TokenPurposeClaim = "token_purpose";
+    public const string CompanySelectionPurpose = "company_selection";
 }
