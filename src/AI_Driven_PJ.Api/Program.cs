5fce8a4b7f70fabbc70d2a5be5b033a1acb8caf3
Su War <suwar14081990@gmail.com>
2026-07-02 13:41:38 +0630

    Update Program.cs

--- src/AI_Driven_PJ.Api/Program.cs
+++ src/AI_Driven_PJ.Api/Program.cs
@@ -2,14 +2,23 @@ using AI_Driven_PJ.Api.Extensions;
 using AI_Driven_PJ.Api.Middlewares;
 using AI_Driven_PJ.Application;
 using AI_Driven_PJ.Infrastructure;
+using Microsoft.AspNetCore.DataProtection;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddCorsConfiguration(builder.Configuration, builder.Environment);
 builder.Services.AddJwtAuthentication(builder.Configuration);
+
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(
+        Path.Combine(builder.Environment.ContentRootPath, "App_Data", "DataProtectionKeys")));
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi(options => options.UseJwtBearerAuthentication());
