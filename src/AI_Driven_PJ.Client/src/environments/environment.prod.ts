58472006f06d2990d99edca7e58a230d7f065abf
Su War <suwar14081990@gmail.com>
2026-07-03 16:15:08 +0630

    Update environment.prod.ts

--- src/AI_Driven_PJ.Client/src/environments/environment.prod.ts
+++ src/AI_Driven_PJ.Client/src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
     production: true,
     apiUrl: '',
-    debug: false
+    main_url: 'https://localhost:7298',
+    web_url: 'https://localhost:7298',
+    debug: false,
+    default_pagination: [10, 20, 30, 50, 100],
+    default_toastKey: 'globalMessage'
 };
