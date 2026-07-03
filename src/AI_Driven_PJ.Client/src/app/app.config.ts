27928b5020cf3ca986235fc1b8d00720a8e3a76a
Su War <suwar14081990@gmail.com>
2026-07-03 16:10:34 +0630

    Update app.config.ts

--- src/AI_Driven_PJ.Client/src/app/app.config.ts
+++ src/AI_Driven_PJ.Client/src/app/app.config.ts
@@ -1,32 +1,55 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling,
+} from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
-import Aura from '@primeuix/themes/aura';
 import { MessageService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
+import RixsFinTrackTheme from './layout/theme/ai-driven-theme';
+import { authInterceptorFn } from '@core/auth/interceptors/auth.interceptor';
+import { httpErrorHandlerInterceptor } from '@core/http/http-error-handler.interceptor';
+import { httpRequestHeaderInterceptor } from '@core/http/http-request-header.interceptor';
+import { encryptHttpRequestInterceptor } from '@core/interceptors/encrypt.interceptor';
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideBrowserGlobalErrorListeners(),
-        provideHttpClient(withFetch()),
-        provideZonelessChangeDetection(),
-        providePrimeNG({
-            theme: {
-                preset: Aura,
-                options: {
-                    darkModeSelector: '.app-dark'
-                }
-            }
-        }),
-        provideTranslateService({
-            loader: provideTranslateHttpLoader({
-                prefix: 'assets/i18n/',
-                suffix: '.json'
-            }),
-            fallbackLang: 'en',
-            lang: 'en'
-        }),
-        MessageService
-    ]
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(
+      routes,
+      withInMemoryScrolling(
+        { anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }),
+      withEnabledBlockingInitialNavigation(),
+    ),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        httpErrorHandlerInterceptor,
+        httpRequestHeaderInterceptor,
+        encryptHttpRequestInterceptor,
+        authInterceptorFn,
+      ]),
+    ),
+    provideZonelessChangeDetection(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: 'assets/i18n/',
+        suffix: '.json',
+      }),
+      fallbackLang: 'en',
+      lang: 'en',
+    }),
+    providePrimeNG({ theme: { preset: RixsFinTrackTheme, options: { darkModeSelector: '.app-dark' } } }),
+    MessageService,
+  ],
 };
