96d2c531bb0a507bcbc0c8b37d8ebb12f0db0fa4
Su War <suwar14081990@gmail.com>
2026-07-03 16:12:46 +0630

    Create companies.routes.ts

---
+++ src/AI_Driven_PJ.Client/src/app/features/companies/companies.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { PlaceholderPage } from '../shared/placeholder.page';
+
+export default [
+  {
+    path: '',
+    component: PlaceholderPage,
+    data: { title: 'MENU.HOME.COMPANIES' },
+  },
+] as Routes;
